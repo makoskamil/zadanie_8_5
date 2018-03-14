2820674851615b82d92d461f76ff18ff831c8c9e
Kamil Makoś <makos.kamil@o2.pl>
2018-03-14 22:05:50 +0100

    Add_files_triangle_field

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = 5,
+    h = 4,
+    triangleArea = a * h / 2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+alert ('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
